d421499a6875ba4554ed51dd3b0f0caa9aec6bef
luizxsoto <luizxsoto@gmail.com>
2022-04-13 07:02:25 -0300

    feat: add customer migration

---
+++ database/migrations/20220207092413_create_customers_table.ts
@@ -0,0 +1,18 @@
+import { createDefaultTable, dropTable } from '@db/helpers';
+import { Knex } from 'knex';
+
+const tableName = 'customers';
+
+export async function up(knex: Knex): Promise<void> {
+  await createDefaultTable(knex, {
+    tableName,
+    columns: (table) => {
+      table.string('name', 100).notNullable();
+      table.string('email', 100).notNullable();
+    },
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await dropTable(knex, { tableName });
+}
