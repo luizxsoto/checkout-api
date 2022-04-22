01387eaa9c12785ba745e341c6a517a0c414315e
luizxsoto <luizxsoto@gmail.com>
2022-04-22 08:16:42 -0300

    refactor: move config folder to shared

--- src/customers/main/factories/use-cases/customer/create.ts
+++ src/customers/main/factories/use-cases/customer/create.ts
@@ -1,7 +1,7 @@
 import { DbCreateCustomerUseCase } from '@/customers/data/use-cases';
 import { CreateCustomerUseCase } from '@/customers/domain/use-cases';
 import { KnexCustomerRepository } from '@/customers/infra/repositories';
-import { knexConfig } from '@/customers/main/config';
+import { knexConfig } from '@/shared/config';
 import { UUIDService } from '@/shared/services';
 
 export function makeDbCreateCustomerUseCase(): CreateCustomerUseCase.UseCase {
