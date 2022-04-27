dd00a540f14812a2bf10480d4f1b68c1f466a3c3
luizxsoto <luizxsoto@gmail.com>
2022-04-27 10:26:33 -0300

    chore: module alias instead of tsc-paths

--- database/knexfile.ts
+++ database/knexfile.ts
@@ -1,3 +1,4 @@
+import 'module-alias/register';
 import { Knex } from 'knex';
 
 import { envConfig } from '@/main/config';
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,4 @@
+import 'module-alias/register';
 import { envConfig, setupApp } from './config';
 
 function bootstrap(): void {
