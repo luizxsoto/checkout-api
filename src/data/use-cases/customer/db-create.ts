1be8e35cc89da0670a6b9d32f88e0b96edbb3b29
luizxsoto <luizxsoto@gmail.com>
2022-04-27 07:29:32 -0300

    feat: ensure validate create customer request

--- src/data/use-cases/customer/db-create.ts
+++ src/data/use-cases/customer/db-create.ts
@@ -1,13 +1,27 @@
-import { CreateCustomerRepository } from '@/data/contracts/repositories';
+import { CreateCustomerRepository, FindByCustomerRepository } from '@/data/contracts/repositories';
+import { CreateCustomerValidator } from '@/data/contracts/validators';
 import { CreateCustomerUseCase } from '@/domain/use-cases';
 
 export class DbCreateCustomerUseCase implements CreateCustomerUseCase.UseCase {
-  constructor(private readonly createCustomerRepository: CreateCustomerRepository.Repository) {}
+  constructor(
+    private readonly createCustomerRepository: CreateCustomerRepository.Repository,
+    private readonly findByCustomerRepository: FindByCustomerRepository.Repository,
+    private readonly createCustomerValidator: CreateCustomerValidator.Validator,
+  ) {}
 
   public async execute(
     requestModel: CreateCustomerUseCase.RequestModel,
   ): Promise<CreateCustomerUseCase.ResponseModel> {
-    const repositoryResult = await this.createCustomerRepository.create(requestModel);
+    const findedCustomers = await this.findByCustomerRepository.findBy({
+      email: requestModel.email,
+    });
+
+    const sanitizedRequestModel = this.createCustomerValidator.validate({
+      requestModel,
+      validatorData: { findedCustomers },
+    });
+
+    const repositoryResult = await this.createCustomerRepository.create(sanitizedRequestModel);
 
     return repositoryResult;
   }
--- src/main/factories/use-cases/customer/create.ts
+++ src/main/factories/use-cases/customer/create.ts
@@ -2,11 +2,13 @@ import { DbCreateCustomerUseCase } from '@/data/use-cases';
 import { CreateCustomerUseCase } from '@/domain/use-cases';
 import { KnexCustomerRepository } from '@/infra/repositories';
 import { UUIDService } from '@/infra/services';
+import { VanillaCreateCustomerValidator } from '@/infra/validators';
 import { knexConfig } from '@/main/config';
 
 export function makeDbCreateCustomerUseCase(): CreateCustomerUseCase.UseCase {
   const repository = new KnexCustomerRepository(knexConfig, new UUIDService());
-  const useCase = new DbCreateCustomerUseCase(repository);
+  const createCustomerValidator = new VanillaCreateCustomerValidator();
+  const useCase = new DbCreateCustomerUseCase(repository, repository, createCustomerValidator);
 
   return useCase;
 }
