9568723fe78d643d774cb91c9f8a7db573649b26
luizxsoto <luizxsoto@gmail.com>
2022-05-06 09:05:41 -0300

    fix: validator name regex

--- src/infra/services/validator.ts
+++ src/infra/services/validator.ts
@@ -70,7 +70,7 @@ export class VanillaValidatorService<
     },
     regex: (key, options: Parameters<Rules['regex']>[0], model) => {
       const regexDict = {
-        name: /^([\w\u00C0-\u00FF]+\s)*[\w\u00C0-\u00FF]+$/,
+        name: /^([a-zA-Z\u00C0-\u00FF]+\s)*[a-zA-Z\u00C0-\u00FF]+$/,
         email: /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/,
         custom: options.customPattern ?? /^\w$/,
       };
