decfd35340681e16499d5169faa246ad407535a9
luizxsoto <luizxsoto@gmail.com>
2022-05-06 09:06:05 -0300

    fix: safety knex toQuery execute

--- src/infra/repositories/knex/base.ts
+++ src/infra/repositories/knex/base.ts
@@ -14,7 +14,7 @@ export abstract class KnexBaseRepository {
   protected async run<T>(query: Knex.QueryBuilder): Promise<T> {
     let queryStr = '';
     try {
-      queryStr = query.toQuery();
+      queryStr = query.toQuery?.();
       return (await query) as T;
     } catch (err) {
       console.error('[KnexBaseRepository.run]', err, queryStr);
