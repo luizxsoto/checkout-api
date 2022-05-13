8d06f5c333cab236728081a4c6eaf367723d7e96
luizxsoto <luizxsoto@gmail.com>
2022-05-13 08:36:02 -0300

    test: ensure return correct values for serverError Case error is instanceof ApplicationException

--- tests/presentation/helpers/http-helper.spec.ts
+++ tests/presentation/helpers/http-helper.spec.ts
@@ -1,21 +1,32 @@
-import { created, ok } from '@/presentation/helpers';
+import { InternalException } from '@/main/exceptions';
+import { created, ok, serverError } from '@/presentation/helpers';
 
 jest.mock('@/main/exceptions/application', () => ({
-  ApplicationException: jest.fn(),
+  ErrorCodes: { INTERNAL: 500 },
+  ApplicationException: jest.fn(() => ({ code: 500 })),
 }));
 
 describe('Http Helpers', () => {
-  test('ok()', () => {
+  test('Should return correct values for ok()', () => {
     const body = { anyProp: 'anyValue' };
     const sutResult = ok(body);
 
     expect(sutResult).toStrictEqual({ statusCode: 200, body });
   });
 
-  test('created()', () => {
+  test('Should return correct values for created()', () => {
     const body = { anyProp: 'anyValue' };
     const sutResult = created(body);
 
     expect(sutResult).toStrictEqual({ statusCode: 201, body });
+  });
+
+  describe('Should return correct values for serverError()', () => {
+    test('Case error is instanceof ApplicationException', () => {
+      const error = new InternalException(new Error());
+      const sutResult = serverError(error);
+
+      expect(sutResult).toStrictEqual({ statusCode: 500, body: error });
+    });
   });
 });
