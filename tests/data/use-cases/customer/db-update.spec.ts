8b1417cd2709225f08c1f958a906fe70eb567164
luizxsoto <luizxsoto@gmail.com>
2022-05-13 10:03:16 -0300

    test: ensure throw ValidationException for every customer invalid prop

--- tests/data/use-cases/customer/db-update.spec.ts
+++ tests/data/use-cases/customer/db-update.spec.ts
@@ -1,6 +1,10 @@
 import { DbUpdateCustomerUseCase } from '@/data/use-cases';
+import { CustomerModel } from '@/domain/models';
+import { ValidationException } from '@/infra/exceptions';
 import { makeCustomerRepositoryStub } from '@tests/data/stubs/repositories';
 import { makeValidatorServiceStub } from '@tests/data/stubs/services';
+
+const validUuidV4 = '00000000-0000-4000-8000-000000000001';
 
 function makeSut() {
   const customerRepository = makeCustomerRepositoryStub();
@@ -15,18 +19,14 @@ describe(DbUpdateCustomerUseCase.name, () => {
     const { customerRepository, validatorService, sut } = makeSut();
 
     const requestModel = {
-      id: '00000000-0000-4000-8000-000000000001',
+      id: validUuidV4,
       name: 'Any Name',
       email: 'any@email.com',
       anyWrongProp: 'anyValue',
     };
     const sanitizedRequestModel = { ...requestModel };
     Reflect.deleteProperty(sanitizedRequestModel, 'anyWrongProp');
-    const responseModel = {
-      ...sanitizedRequestModel,
-      id: '00000000-0000-4000-8000-000000000001',
-      updatedAt: new Date(),
-    };
+    const responseModel = { ...sanitizedRequestModel, updatedAt: new Date() };
 
     customerRepository.findBy.mockReturnValueOnce([responseModel]);
     customerRepository.update.mockReturnValueOnce(responseModel);
@@ -73,4 +73,120 @@ describe(DbUpdateCustomerUseCase.name, () => {
     );
     expect(customerRepository.findBy).toBeCalledWith({ email: sanitizedRequestModel.email });
   });
+
+  describe.each([
+    // id
+    {
+      properties: { id: undefined, email: 'any@email.com' },
+      validations: [{ field: 'id', rule: 'required', message: 'This value is required' }],
+    },
+    {
+      properties: { id: 1, email: 'any@email.com' },
+      validations: [{ field: 'id', rule: 'string', message: 'This value must be a string' }],
+    },
+    {
+      properties: { id: 'invalid_uuid', email: 'any@email.com' },
+      validations: [
+        {
+          field: 'id',
+          rule: 'regex',
+          message: 'This value must be valid according to the pattern: uuidV4',
+        },
+      ],
+    },
+    {
+      properties: { id: 'not_found_id', email: 'any@email.com' },
+      validations: [{ field: 'id', rule: 'exists', message: 'This value was not found' }],
+    },
+    // name
+    {
+      properties: { name: 1, email: 'any@email.com' },
+      validations: [{ field: 'name', rule: 'string', message: 'This value must be a string' }],
+    },
+    {
+      properties: { name: ' InV@L1D n@m3 ', email: 'any@email.com' },
+      validations: [
+        {
+          field: 'name',
+          rule: 'regex',
+          message: 'This value must be valid according to the pattern: name',
+        },
+      ],
+    },
+    {
+      properties: { name: 'lower', email: 'any@email.com' },
+      validations: [
+        { field: 'name', rule: 'length', message: 'This value length must be beetween 6 and 100' },
+      ],
+    },
+    {
+      properties: {
+        name: 'BiggestName BiggestName BiggestName BiggestName BiggestName BiggestName BiggestName BiggestName BiggestName BiggestName',
+        email: 'any@email.com',
+      },
+      validations: [
+        { field: 'name', rule: 'length', message: 'This value length must be beetween 6 and 100' },
+      ],
+    },
+    // email
+    {
+      properties: { email: 1 },
+      validations: [{ field: 'email', rule: 'string', message: 'This value must be a string' }],
+    },
+    {
+      properties: { email: ' InV@L1D eM@1L ' },
+      validations: [
+        {
+          field: 'email',
+          rule: 'regex',
+          message: 'This value must be valid according to the pattern: email',
+        },
+      ],
+    },
+    {
+      properties: {
+        email:
+          'biggest_email_biggest_email_biggest_email_biggest_email_biggest_email_biggest_email_biggest_email@invalid.com',
+      },
+      validations: [
+        { field: 'email', rule: 'length', message: 'This value length must be beetween 6 and 100' },
+      ],
+    },
+    {
+      properties: { email: 'valid@email.com' },
+      validations: [
+        { field: 'email', rule: 'unique', message: 'This value has already been used' },
+      ],
+    },
+  ])(
+    'Should throw ValidationException for every customer invalid prop',
+    ({ properties, validations }) => {
+      it(JSON.stringify(validations), async () => {
+        const { customerRepository, sut } = makeSut();
+
+        // eslint-disable-next-line prefer-object-spread
+        const requestModel: CustomerModel = Object.assign(
+          { id: validUuidV4, name: 'Any Name', email: 'any@email.com' },
+          properties,
+        );
+        requestModel.id =
+          properties.id === 'not_found_id'
+            ? '00000000-0000-4000-8000-000000000002'
+            : requestModel.id;
+        const responseModel = { ...requestModel, updatedAt: new Date() };
+
+        customerRepository.findBy.mockReturnValueOnce([
+          {
+            ...responseModel,
+            id: properties.id === 'not_found_id' ? validUuidV4 : responseModel.id,
+          },
+        ]);
+        customerRepository.update.mockReturnValueOnce(responseModel);
+
+        const sutResult = await sut.execute(requestModel).catch((e) => e);
+
+        expect(sutResult).toStrictEqual(new ValidationException(validations));
+      });
+    },
+  );
 });
