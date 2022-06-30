fbd2e4817aaeac308b5dcb8ac28a5e6dbb539a9c
luizxsoto <luizxsoto@gmail.com>
2022-06-30 07:26:42 -0300

    feat: produt migrations and seeds
    #7

---
+++ database/migrations/20220629120148_create_products_table.ts
@@ -0,0 +1,20 @@
+import { Knex } from 'knex';
+
+import { createDefaultTable, dropTable } from '../helpers';
+
+const tableName = 'products';
+
+export async function up(knex: Knex): Promise<void> {
+  await createDefaultTable(knex, {
+    tableName,
+    columns: (table) => {
+      table.string('name', 255).notNullable();
+      table.string('category', 100).notNullable();
+      table.integer('price').notNullable();
+    },
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await dropTable(knex, { tableName });
+}
---
+++ database/migrations/20220629120149_insert_products.ts
@@ -0,0 +1,48 @@
+import { Knex } from 'knex';
+
+import { ProductModel } from '@/domain/models';
+import { envConfig } from '@/main/config';
+
+const tableName = 'products';
+const products: (Omit<ProductModel, 'createdAt'> & {
+  createdAt: string;
+})[] = [
+  {
+    id: '00000000-0000-4000-8000-000000000001',
+    createUserId: '00000000-0000-4000-8000-000000000001',
+    createdAt: new Date().toISOString(),
+    name: 'Product 1',
+    category: 'clothes',
+    price: 1000,
+  },
+  {
+    id: '00000000-0000-4000-8000-000000000002',
+    createUserId: '00000000-0000-4000-8000-000000000001',
+    createdAt: new Date().toISOString(),
+    name: 'Product 2',
+    category: 'shoes',
+    price: 2000,
+  },
+  {
+    id: '00000000-0000-4000-8000-000000000003',
+    createUserId: '00000000-0000-4000-8000-000000000001',
+    createdAt: new Date().toISOString(),
+    name: 'Product 3',
+    category: 'others',
+    price: 3000,
+  },
+];
+
+export async function up(knex: Knex): Promise<void> {
+  if (envConfig.nodeEnv === 'production') return;
+
+  await knex.table(tableName).insert(products);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  if (envConfig.nodeEnv === 'production') return;
+
+  const productIds = products.map((product) => product.id);
+
+  await knex.table(tableName).whereIn('id', productIds).delete();
+}
