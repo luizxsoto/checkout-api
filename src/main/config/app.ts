f1ba682fb8fe737f067331732a777b84b85328c7
luizxsoto <luizxsoto@gmail.com>
2022-07-30 09:20:02 -0300

    feat: ensure setup swagger
    #12

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -4,10 +4,12 @@ import { envConfig } from './env'
 import { setupInterceptors } from './interceptors'
 import { setupMiddlewares } from './middlewares'
 import { setupRoutes } from './routes'
+import { setupSwagger } from './swagger'
 
 export function setupApp(setupNotFoundRoute = envConfig.nodeEnv !== 'test'): Express {
   const app = express()
 
+  setupSwagger(app)
   setupMiddlewares(app)
   setupRoutes(app, setupNotFoundRoute)
   setupInterceptors(app)
---
+++ src/main/config/swagger.ts
@@ -0,0 +1,9 @@
+import { Express } from 'express'
+import { serve, setup } from 'swagger-ui-express'
+
+import { swaggerConfig } from '@/main/docs'
+import { noCache } from '@/main/middlewares'
+
+export function setupSwagger(app: Express): void {
+  app.use('/api/docs', noCache, serve, setup(swaggerConfig))
+}
