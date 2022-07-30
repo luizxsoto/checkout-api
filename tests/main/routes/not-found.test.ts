18258119b750e28ce79af6db229beb0d0e7d0b2a
luizxsoto <luizxsoto@gmail.com>
2022-07-30 09:25:49 -0300

    test: ensure return normalized body when route is not found

--- tests/main/routes/not-found.test.ts
+++ tests/main/routes/not-found.test.ts
@@ -2,8 +2,10 @@ import { Express } from 'express'
 import request from 'supertest'
 
 import { setupApp } from '@/main/config'
+import { NotFoundException } from '@/main/exceptions'
 
 let app: Express
+const notFoundExceptionObject = { ...new NotFoundException() }
 
 describe('Not Found Route', () => {
   beforeAll(() => {
@@ -13,18 +15,18 @@ describe('Not Found Route', () => {
   test('Should return a correct json for a route not found for all methods', async () => {
     let result = await request(app).get('/route-not-found').send()
     expect(result.status).toBe(404)
-    expect(result.body.message).toBe('Route not found')
+    expect(result.body).toStrictEqual(notFoundExceptionObject)
 
     result = await request(app).post('/route-not-found').send()
     expect(result.status).toBe(404)
-    expect(result.body.message).toBe('Route not found')
+    expect(result.body).toStrictEqual(notFoundExceptionObject)
 
     result = await request(app).put('/route-not-found').send()
     expect(result.status).toBe(404)
-    expect(result.body.message).toBe('Route not found')
+    expect(result.body).toStrictEqual(notFoundExceptionObject)
 
     result = await request(app).delete('/route-not-found').send()
     expect(result.status).toBe(404)
-    expect(result.body.message).toBe('Route not found')
+    expect(result.body).toStrictEqual(notFoundExceptionObject)
   })
 })
